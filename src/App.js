69bc3d98338ff1fd2ea510bdbfdf7eaff8b24c08
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-17 20:48:56 +0530

    add MealsSummary component and AvailableMeals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./Components/Layout/Header";
 import React, { Fragment } from "react";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
